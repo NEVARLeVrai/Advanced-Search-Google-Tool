ee052705f6232e33e6ce0604d49c593b3a134332
NΞVΛR <danielo-24@hotmail.com>
2024-03-15 20:24:51 +0100

    test

--- Advanced Search Google Tool.Designer.vb
+++ Advanced Search Google Tool.Designer.vb
@@ -22,12 +22,14 @@ Partial Class AdvancedSearch
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AdvancedSearch))
         Me.loginBouton2 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.loginBouton = New System.Windows.Forms.Button()
         Me.intileText = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.intileBouton = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'loginBouton2
@@ -84,17 +86,31 @@ Partial Class AdvancedSearch
         Me.intileBouton.Text = "Envoyer"
         Me.intileBouton.UseVisualStyleBackColor = True
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(730, 428)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "V1.0 alpha"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
         'AdvancedSearch
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.loginBouton2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.loginBouton)
         Me.Controls.Add(Me.intileText)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.intileBouton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "AdvancedSearch"
         Me.Text = "Advanced Search Google Tool"
         Me.ResumeLayout(False)
@@ -108,4 +124,5 @@ Partial Class AdvancedSearch
     Friend WithEvents intileText As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents intileBouton As Button
+    Friend WithEvents Label3 As Label
 End Class
