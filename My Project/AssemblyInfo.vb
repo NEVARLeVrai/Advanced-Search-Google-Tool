437554bce8419c47bd73fe2d62841c888c5b92df
NΞVΛR <danielo-24@hotmail.com>
2024-03-15 20:51:32 +0100

    test

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -11,10 +11,10 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("Advanced Search Google Tool")>
 <Assembly: AssemblyDescription("This application that enables users to perform advanced searches using specialized operators. It offers a user-friendly interface, customizable queries, search result previews, and export functionality")>
-<Assembly: AssemblyCompany("NEVARLeVrai")>
+<Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("Advanced Search Google Tool")>
-<Assembly: AssemblyCopyright("NEVARLeVrai")>
-<Assembly: AssemblyTrademark("NEVARLeVrai")>
+<Assembly: AssemblyCopyright("NEVARLeVrai check on Github")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
